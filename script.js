512e1a410431a696713b4fcf142d4c7b14c5af56
Mark Jobs <doniyor.doni@mail.ru>
2022-08-24 22:02:53 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// const questionOne = confirm("Вы готовы?"); 
+
+    
+// if (questionOne == true) {
+    
+//     const questionTwo = prompt("Укажите свой возвраст!", "");
+//     if (questionTwo > 18) 
+//     alert('Welcome!');
+//     else
+//     alert('Good bay!');
+// } if (questionOne == false)
+//     alert('.!.');
+    
+
+
+// const a = 1;
+// let   banana = 1;
+//     if (a === banana)
+//     console.log(true);
+//     else
+//     console.log(false);
+//     if (a == banana)
+//     console.log(true);
+//     else
+//     console.log(false);
+
+const one = {
+    a: 'twoOne'
+};
+      two = [...one(a)];
+
+console.log(two);
+
+
